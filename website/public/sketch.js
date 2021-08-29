c619da461fd5b66b7780033f1adc3416579d0f18
TomaszSorobka <tomek.sorobka@gmail.com>
2021-08-29 15:58:26 +0200

    update

--- website/public/sketch.js
+++ website/public/sketch.js
@@ -13,6 +13,16 @@ let workingarray =[];
 let oddoreven = 1;
 let index, index2, pricevar, stockvar, wartoscvar;
 
+const date = new Date();
+  let day = date.getDate();
+  if(day<10) day = '0'+day;
+  let month = date.getMonth() +1;
+  if(month<10) month = '0'+month;
+  let year = date.getFullYear();
+  let UTCdate = year.toString() +'-'+month.toString()+'-'+day.toString();
+
+tod = UTCdate;
+
       async function getData() {
         const response = await fetch('/api');
         //fetching the db data
