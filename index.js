809c721af00d94c350c1f2943fecf672c0021ff8
TomaszSorobka <tomek.sorobka@gmail.com>
2021-08-30 16:52:20 +0200

    changes to db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Datastore = require('nedb');
+const mysql = require('mysql');
 require('dotenv').config();
 
 const app = express();
@@ -7,19 +8,39 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening at ${port}`));
 app.use(express.static('website/public'));
 
+var pool = mysql.createPool({
+    host: 'remotemysql.com',
+    port: '3306', 
+    user: 'v2AqjSz2oR', 
+    password: 'MuvdDi9iwR',
+    database: 'v2AqjSz2oR',
+    connectionLimit: '100',
+    multipleStatements: true
+});
+
 const database = new Datastore('database.db');
 database.loadDatabase();
 
 
 app.get('/api', (request, response) => {
-    database.find({}, (err, data) => {
+    // database.find({}, (err, data) => {
+    //     if (err) {
+    //         response.end();
+    //         console.log('error')
+    //         return;
+    //     }
+    //     response.json(data);
+    // });
+
+    pool.query('select * from Main', function(err, data){
         if (err) {
-            response.end();
-            console.log('error')
-            return;
-        }
-        response.json(data);
-    });
+                     response.end();
+                     console.log('error')
+                     return;
+                 }
+                 response.json(data);
+            });
+     })
     
-});
+
 
